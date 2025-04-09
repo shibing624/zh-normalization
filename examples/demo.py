81bd07822afaaa55dcf963a74bb45a95b7fbf83e
xyzwps <xyzwps@outlook.com>
2025-04-10 00:59:30 +0800

    support fullwidth percent sign

--- examples/demo.py
+++ examples/demo.py
@@ -7,10 +7,9 @@ import sys
 
 sys.path.append('..')
 from zh_normalization import TextNormalizer
-
 if __name__ == '__main__':
     m = TextNormalizer()
-    text = "电影中梁朝伟扮演的陈永仁的编号27149!啊——但是《原神》是由,米哈\游自主，研发的一款全.新开放世界.冒险游戏.呣呣呣～就是…大人的鼹鼠党吧？"
+    text = "电影中梁朝伟扮演的陈永仁的编号27149!啊——但是《原神》是由,米哈\游100％自主，研发的一款全.新开放世界.冒险游戏.呣呣呣～就是…大人的鼹鼠党吧？"
     sents = m.normalize(text)
     new_text = ''.join(sents)
     print(text)
