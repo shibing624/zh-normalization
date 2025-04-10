78ebee73ab95ef0615f82a7baabc8423618852c5
shibing624 <shibing624@126.com>
2025-04-10 11:49:51 +0800

    update version 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='zh-normalization',
-    version='0.0.1',
+    version='0.0.2',
     description='Chinese Text Normalization(for speech recognition and text to speech)',
     long_description=readme,
     long_description_content_type='text/markdown',
